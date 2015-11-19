cdaf24f6fe00f009f72439ecef9951154dc7cc84
oliver.ye <yeming0310@qq.com>
2015-11-19 21:44:45 +0800

    bc

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -9,4 +9,17 @@ router.get('/', function(req, res, next) {
 router.get('/reg',function(req,res){
 	res.render('reg',{title:'注册'})
 });
+
+ router.get('/login', function (req, res) {
+    res.render('login', { title: '登录' });
+  });
+  router.post('/login', function (req, res) {
+  });
+  router.get('/post', function (req, res) {
+    res.render('post', { title: '发表' });
+  });
+  router.post('/post', function (req, res) {
+  });
+  router.get('/logout', function (req, res) {
+  });
 module.exports = router;
